ca1225ac1f9353db892957db53f9975ebbfb2bbf
Darksrhacking <darksrhacking@gmail.com>
2025-04-27 15:02:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=41p23QjJ#_MegmYOMq1aD4gYMOeiPreXSnsHQxm2pWT4eWHx5QnM",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
